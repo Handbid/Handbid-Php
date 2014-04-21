b4a1f55623e2903df55caa9854ac8d214385e325
Tay <me@taylorrome.ro>
2014-04-21 13:51:10 -0600

    auction byOrg

--- src/Store/Auction.php
+++ src/Store/Auction.php
@@ -9,4 +9,15 @@ class Auction extends StoreAbstract
 
     public $_base = 'auctions';
 
+    /**
+     * Auctions by org, simply stubbed out for now.
+     *
+     * @param $id
+     * @return mixed
+     */
+    public function byOrg($id)
+    {
+        return $this->all(1, 2);
+    }
+
 }
