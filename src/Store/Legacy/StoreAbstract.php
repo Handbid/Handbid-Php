5e94ac4cf6e7bd73412b23471160b378cbab305b
Hemstreet <jonhemstreet@gmail.com>
2014-09-15 14:53:43 -0600

    exception removal from store abstract

--- src/Store/Legacy/StoreAbstract.php
+++ src/Store/Legacy/StoreAbstract.php
@@ -73,10 +73,6 @@ class StoreAbstract implements StoreInterface
         $results = $this->_rest->get($this->_base, $query);
         $results = $results ? $results->{$this->_resultsKeyPlural} : [];
 
-        if (count($results) == 0) {
-            throw new \Handbid\Exception\Network('Could not find entity with key ' . $key);
-        }
-
         return $this->map($results[0]);
     }
 
