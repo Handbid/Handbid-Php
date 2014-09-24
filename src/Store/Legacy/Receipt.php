aa2f44ce4d62d00d9e21c46113ddcf77988123ac
Hemstreet <jonhemstreet@gmail.com>
2014-09-24 14:36:40 -0600

    receipt support

---
+++ src/Store/Legacy/Receipt.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Handbid\Store\Legacy;
+
+use Handbid\Store\Legacy\StoreAbstract;
+
+class Receipt extends StoreAbstract
+{
+    public $_base = 'models/Receipt';
+    public $_resultKey = 'Receipt';
+    public $_receiptCache = [];
+
+    public function byAuction($auctionId)
+    {
+
+        if (!isset($this->_receiptCache[$auctionId])) {
+
+            $this->_receiptCache[$auctionId] = $this->_rest->get(
+                'receipt',
+                [
+                    'query' => [
+                        'auction' => $auctionId
+                    ]
+                ]
+            );
+        }
+
+        return $this->_receiptCache[$auctionId];
+    }
+}
