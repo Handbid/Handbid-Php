1613f80a909b42733854472b316a4b0a878563f4
Tay <me@taylorrome.ro>
2014-09-24 13:42:45 -0600

    auction sorting additions

--- src/Store/Legacy/Auction.php
+++ src/Store/Legacy/Auction.php
@@ -10,18 +10,51 @@ class Auction extends StoreAbstract
     public $_base = 'models/Auction';
     public $_resultsKey = 'Auction';
 
+    public function _queryUpcoming()
+    {
+        return ['startTime' => ['$gt' => time() - 3600]];
+    }
+
+    public function _queryPast()
+    {
+        return ['startTime' => ['$lt' => time() - 3600]];
+    }
+
+    public function _queryCurrent()
+    {
+        return ['status' => ['$in' => ['open', 'extended', 'preview', 'presale', 'ending']]];
+    }
+
+    public function _queryOpen()
+    {
+        return ['status' => ['$in' => ['open', 'extended', 'ending']]];
+    }
+
+    public function _queryClosed()
+    {
+        return ['status' => 'closed'];
+    }
+
+    public function _queryPreview()
+    {
+        return ['status' => 'presale'];
+    }
+
+    public function _queryPresale()
+    {
+        return ['status' => 'presale'];
+    }
+
     public function byOrg($page = 0, $pageSize = 25, $sortField = 'name', $sortDirection = 'ASC', $id)
     {
-
         $query = ['organization' => $id];
         return $this->all($page, $pageSize, $sortField, $sortDirection, $query);
-
     }
 
     public function upcoming($page = 0, $pageSize = 25, $sortField = 'name', $sortDirection = 'ASC', $orgId = '')
     {
 
-        $query = ['startTime' => ['$gt' => time() - 3600]];
+        $query = $this->_queryUpcoming();
 
         if ($orgId) {
             $query['organization'] = $orgId;
@@ -32,13 +65,57 @@ class Auction extends StoreAbstract
 
     public function past($page = 0, $pageSize = 25, $sortField = 'name', $sortDirection = 'ASC', $orgId = '')
     {
-        $query = ['startTime' => ['$lt' => time() - 3600]];
+        $query = $this->_queryPast();
 
         if ($orgId) {
             $query['organization'] = $orgId;
         }
 
         return $this->all($page, $pageSize, $sortField, $sortDirection, $query);
+    }
+
+    public function byStatus($status, $page = 0, $pageSize = 25, $sortField = 'name', $sortDirection = 'ASC', $orgId = '')
+    {
+
+        $query = $this->{'_query' . ucfirst($status)}();
+
+        if ($orgId) {
+            $query['organization'] = $orgId;
+        }
+
+        return $this->all($page, $pageSize, $sortField, $sortDirection, $query);
+
+    }
+
+    public function current($page = 0, $pageSize = 25, $sortField = 'name', $sortDirection = 'ASC', $orgId = '')
+    {
+        $query = $this->_queryCurrent();
+
+        if ($orgId) {
+            $query['organization'] = $orgId;
+        }
+
+        return $this->all($page, $pageSize, $sortField, $sortDirection, $query);
+    }
+
+    public function open($page = 0, $pageSize = 25, $sortField = 'name', $sortDirection = 'ASC', $orgId = '')
+    {
+        return $this->byStatus('open', $page, $pageSize, $sortField, $sortDirection, $orgId);
+    }
+
+    public function closed($page = 0, $pageSize = 25, $sortField = 'name', $sortDirection = 'ASC', $orgId = '')
+    {
+        return $this->byStatus('closed', $page, $pageSize, $sortField, $sortDirection, $orgId);
+    }
+
+    public function preview($page = 0, $pageSize = 25, $sortField = 'name', $sortDirection = 'ASC', $orgId = '')
+    {
+        return $this->byStatus('preview', $page, $pageSize, $sortField, $sortDirection, $orgId);
+    }
+
+    public function presale($page = 0, $pageSize = 25, $sortField = 'name', $sortDirection = 'ASC', $orgId = '')
+    {
+        return $this->byStatus('presale', $page, $pageSize, $sortField, $sortDirection, $orgId);
     }
 
     public function map($entity)
@@ -57,9 +134,12 @@ class Auction extends StoreAbstract
 
     }
 
-    public function count($query = [])
+    public function count($type = 'all')
     {
-        return $this->_rest->get('auctions/count', $query)->count;
+
+        $query = $this->{'_query' . ucfirst($type)}();
+
+        return $this->_rest->get('auctions/count', ['query' => $query])->count;
     }
 
     public function myRecent($query = [])
