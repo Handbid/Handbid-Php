33859f4819aad0cd87c6a43ba140bdf9ea652176
Tay <me@taylorrome.ro>
2014-10-27 15:59:23 -0600

    notification store

---
+++ src/Store/Legacy/Notification.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Handbid\Store\Legacy;
+
+use Handbid\Store\Legacy\StoreAbstract;
+
+class Notification extends StoreAbstract
+{
+    public $_base = 'bidder/notifications';
+    public $_resultsKey = 'Message';
+
+
+}
